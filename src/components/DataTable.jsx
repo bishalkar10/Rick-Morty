829a03881d6e76f62b70719c8ea25fcd91a8b7a8
Bishal kar <bishalkare10@gmail.com>
2024-07-05 18:40:00 +0530

    location, gender, and species are now links

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,16 +1,33 @@
-export function DataTable(data) {
-  return (
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
+export function DataTable(data) {
+
+  return (
     <table>
       <tbody>
-        {Object.entries(data).map(([key, value], index) => (
-          <tr key={index}>
-            <th>{key}</th>
-            <td>{value}</td>
-          </tr>
-        ))}
+        {/* Wrap species and gender in a Link */}
+        <tr>
+          <th>Species</th>
+          <td><Link to={`/characters?species=${data.species}`}>{data.species}</Link></td>
+        </tr>
+        <tr>
+          <th>Gender</th>
+          <td><Link to={`/characters?gender=${data.gender}`}>{data.gender}</Link></td>
+        </tr>
+        {/* Format location with a dynamic ID */}
+        <tr>
+          <th>Location</th>
+          <td><Link to={`/locations/${data.locationID}`}>{data.location}</Link></td>
+        </tr>
+        <tr>
+          <th>Dimension</th>
+          <td>{data.dimension}</td>
+        </tr>
+        <tr>
+          <th>Amount Of Residents</th>
+          <td>{data.amountOfResidents}</td>
+        </tr>
       </tbody>
     </table>
-
   );
 }
