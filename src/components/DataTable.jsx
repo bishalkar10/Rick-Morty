c48c13fb903f028a4cfb85b1f3447436504befe5
Bishal kar <bishalkare10@gmail.com>
2024-07-05 20:09:53 +0530

    added missing 'origin' row

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,20 +1,23 @@
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 export function DataTable(data) {
 
   return (
     <table>
       <tbody>
-        {/* Wrap species and gender in a Link */}
+        
         <tr>
           <th>Species</th>
-          <td><Link to={`/characters?species=${data.species}`}>{data.species}</Link></td>
+          <td><Link to={`/characters?species=${data.species.toLowerCase()}`}>{data.species}</Link></td>
         </tr>
         <tr>
           <th>Gender</th>
-          <td><Link to={`/characters?gender=${data.gender}`}>{data.gender}</Link></td>
+          <td><Link to={`/characters?gender=${data.gender.toLowerCase()}`}>{data.gender}</Link></td>
         </tr>
-        {/* Format location with a dynamic ID */}
+        <tr>
+          <th>Origin</th>
+          <td>{data.origin}</td>
+        </tr>
         <tr>
           <th>Location</th>
           <td><Link to={`/locations/${data.locationID}`}>{data.location}</Link></td>
